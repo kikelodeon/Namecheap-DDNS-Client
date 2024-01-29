2cd3b918ce029542f7fb3a7dce188a146f79f7dd
Quique <quiquesg88@gmail.com>
2024-01-29 01:19:43 +0100

    save

--- NameCheapDDNSClient/obj/Debug/net8.0/NameCheapDDNSClient.AssemblyInfo.cs
+++ NameCheapDDNSClient/obj/Debug/net8.0/NameCheapDDNSClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NameCheapDDNSClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0f2533308a7189e0f2c89df2e25a568c399e7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3705c3cf0081664e717a077acb4dd62ce523345d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NameCheapDDNSClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NameCheapDDNSClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
